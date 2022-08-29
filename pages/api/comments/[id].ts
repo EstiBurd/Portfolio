9bfe736e94f84a2ddc2684a67131d90035192d2c
EstiBurd <e0583222839@gmail.com>
2022-08-30 01:46:39 +0300

    30-08-2022

--- pages/api/comments/[id].ts
+++ pages/api/comments/[id].ts
@@ -1,16 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import IComment from 'src/types/IComment';
-import { getComments } from 'src/utils/server/comments-storage';
+import { deleteComment, getComment } from 'src/utils/server/comments-storage';
 
-function getComment(id:number):IComment|undefined{
-  return  getComments().find(it=> it.id==id)
-}
+
 export default function handlCommentWithId(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-    const id=Number(req.query.id);
-    const comment=getComment(id);
-    comment?res.status(200).json(comment):res.status(404).send("");
+  const id = Number(req.query.id);
+  switch (req.method) {
+    case 'GET':
+      const comment = getComment(id);
+      comment ? res.status(200).json(comment) : res.status(404).send("");
+      break;
+    case 'DELETE':
+const isDelete=deleteComment(id);
+isDelete?res.status(200).send(""):res.status(404).send("");
+    default:
+      throw `Unexpected request method :${req.method}`
+  }
 }
--- src/utils/server/comments-storage.ts
+++ src/utils/server/comments-storage.ts
@@ -4,6 +4,18 @@ import comments from 'data/comments.json';
 import IComment from 'src/types/IComment';
 import ICommentShort from 'src/types/ICommentShort';
 
+export function deleteComment(id:number):boolean{
+   const indexFound:number= comments.findIndex(comment=>comment.id==id)
+if(indexFound<0){
+    return false;
+}
+comments.splice(indexFound,1);
+saveComments();
+return true;
+}
+export function getComment(id: number): IComment | undefined {
+    return getComments().find(it => it.id == id)
+  }
 export function getCommentsShort():ICommentShort[]{
     return comments.map(comment=>{
         const{id,description}=comment;
@@ -17,7 +29,7 @@ export function getComments():IComment[]{
 }
 export function saveComments():void{
     const data=JSON.stringify(comments);
-    fs.writeFileSync(path.join('data','comments.lson'),data)
+    fs.writeFileSync(path.join('data','comments.json'),data)
     }
     export function addComment(newComment:IComment):void{
         comments.push(newComment);
